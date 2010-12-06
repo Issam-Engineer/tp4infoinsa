d7648516cb7b8a01e80afda710c4ba3ff49553e4
havez.maxime.01 <havez.maxime.01@9f3b02c3-fd90-5378-97a3-836ae78947c6>
2010-12-06 20:53:58 +0000

    on a pas mal de fonctions d'accès à définir ...
    Pour ce qui est de l'asso moteur facade, il s'agit d'une asso bidirectionnelle donc on doit mettre deux pointeur (c fait) et le execute de facade permet d'appeler le execute de moteur c'est ce qui permet de pas faire de l'attente active en fait! 
    
    Autre nota, avec le wrapper c cho de passer un tableau (genre pair<int,int> [] car c'est pas un type primitif 
    
    Voilà qqe sujets à méditer ! ^^

--- trunk/TPs CPOO/Gareth & Maxime/Projet/CanonNoir_Moteur_C++/fichiers/AttenteNbJoueurs.cpp
+++ trunk/TPs CPOO/Gareth & Maxime/Projet/CanonNoir_Moteur_C++/fichiers/AttenteNbJoueurs.cpp
@@ -9,9 +9,12 @@
 #include "AttenteNbJoueurs.h"
 #include "AttenteInitialisation.h"
 
+
+
 void AttenteNbJoueurs::execute(){
-	while(Facade::nb_Joueurs == NULL);
-	motor.nbJoueurs = ... ; 
-	motor.courant = new AttenteInitialisation();
-	motor.execute();
+	if((motor.facade)->nb_Joueurs==2 || motor.facade->nb_Joueurs==3 || motor.facade->nb_Joueurs==4){
+		motor.nbJoueurs = motor.facade->nb_Joueurs;
+		motor.courant = new AttenteInitialisation();
+	}else{
+		cerr<<"ERROR : AttenteNbJoueurs"<<endl;
 }
