e6cacffc91a5f52a1a9d2fd19a20c0aa636f276a
havez.maxime.01 <havez.maxime.01@9f3b02c3-fd90-5378-97a3-836ae78947c6>
2010-12-22 22:30:40 +0000

    [retouches et commencement de l'état déplacement]

---
+++ trunk/TPs CPOO/Gareth & Maxime/Projet/CanonNoir_Moteur_C++/CanonNoirMoteur/CanonNoirMoteur/AttenteDeplacement.cpp
@@ -0,0 +1,15 @@
+/**
+*\file AttenteDeplacement.h
+*\brief File containing the functionalities and the attributes of the AttenteDeplacement class
+*\author Maxime HAVEZ
+*\author Gareth THIVEUX
+*\version 1.0
+*/
+#include "AttenteDeplacement.h"
+
+void AttenteDeplacement::execute(){
+	//vérifier qu'il a cliqué sur une case correcte
+
+	//modifier les valeur : positBateau... et faire un case.getState()
+
+}
